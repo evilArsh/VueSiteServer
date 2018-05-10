a338616da837d3b78ff3f1c98e4b885439d72629
evilArsh <1183520543@qq.com>
2018-05-10 10:12:57 +0800

    部署分离

---
+++ config/config.local.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = appInfo => {
+    const config = exports = {};
+    config.mysql = {
+        // mysql config
+        client: {
+            host: 'localhost',
+            port: '3306',
+            user: 'root',
+            password: '',
+            database: 'web',
+        },
+        // 是否加载到 app 上，默认开启
+        app: true,
+        // 是否加载到 agent 上，默认关闭
+        agent: true,
+    };
+    // Nginx反向代理
+    config.proxy = false;
+      //跨站白名单
+  // session && cookie, (havn't been used)
+  config.security = {
+    csrf: {
+      useSession: false, // 默认为 false，当设置为 true 时，将会把 csrf token 保存到 Session 中
+      cookieName: '_csrf', // Cookie 中的字段名，默认为 csrfToken
+      sessionName: '_csrf', // Session 中的字段名，默认为 csrfToken
+      headerName: 'x-csrf-token',
+      ignoreJSON: false
+    },
+    domainWhiteList: ['http://localhost:8080', 'http://localhost', 'http://localhost:80', 'http://127.0.0.1']
+  };
+    return config;
+}
